77fa9450e65768727e6ba81d820b059ac6239b26
zippy0626 <zippy0626@gmail.com>
2025-08-02 10:57:24 -0400

    Added DOM selection logic, event listener logic + callbacks

---
+++ habit-tracker/C11/zippy0626/src/modules/View.js
@@ -0,0 +1,63 @@
+const View = {
+  getSearchBar() {
+    return document.querySelector("#search-bar");
+  },
+
+  getAddHabitBtn() {
+    return document.querySelector(".user-add-habit");
+  },
+
+  getFiltersBtn() {
+    return document.querySelector(".user-filters");
+  },
+
+  getModalOverlay() {
+    return document.querySelector(".modal-overlay");
+  },
+
+  getCreateHabitBtn() {
+    return document.querySelector(".create-habit-btn");
+  },
+
+  getCancelHabitBtn() {
+    return document.querySelector(".cancel-habit-btn");
+  },
+
+  showAddHabitModal() {
+    const addHabitModal = document.querySelector(".add-habit-modal");
+    const overlay = document.querySelector(".modal-overlay");
+    overlay.classList.remove("hidden");
+    addHabitModal.classList.remove("hidden");
+  },
+
+  hideAddHabitModal() {
+    const addHabitModal = document.querySelector(".add-habit-modal");
+    const overlay = document.querySelector(".modal-overlay");
+    overlay.classList.add("hidden");
+    addHabitModal.classList.add("hidden");
+  },
+
+  clearAddHabitForm() {
+    document.getElementById("add-habit-modal-form").reset();
+  },
+
+  // event listener callback fns for Controller
+  // handle add-habit modal related
+  onAddHabitClick(callback) {
+    this.getAddHabitBtn().addEventListener("click", callback);
+  },
+
+  onModalOverlayClick(callback) {
+    this.getModalOverlay().addEventListener("click", callback);
+  },
+
+  onCreateHabitClick(callback) {
+    this.getCreateHabitBtn().addEventListener("click", callback);
+  },
+
+  onCancelHabitClick(callback) {
+    this.getCancelHabitBtn().addEventListener("click", callback);
+  },
+};
+
+export default View;
