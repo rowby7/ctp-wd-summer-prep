8d4cb8c11085e736b0a4be31c349e87a17f81ed3
zippy0626 <zippy0626@gmail.com>
2025-08-02 12:15:10 -0400

    refactor Controller to pass callbacks instead of managing DOM

--- habit-tracker/C11/zippy0626/src/modules/Controller.js
+++ habit-tracker/C11/zippy0626/src/modules/Controller.js
@@ -1,40 +1,29 @@
+import View from "./View.js";
+
 const Controller = {
   init() {
-    this.handleUserToolButtons()
-    this.handleModalClose()
+    View.onAddHabitClick(() => {
+      View.clearAddHabitForm();
+      View.showAddHabitModal();
+    });
+
+    View.onModalOverlayClick(() => {
+      View.clearAddHabitForm();
+      View.hideAddHabitModal();
+    });
+
+    View.onCreateHabitClick((event) => {
+      event.preventDefault();
+      View.hideAddHabitModal();
+      // do some stuff with Model.js here
+      const data = View.getAddHabitFormData()
+    });
+
+    View.onCancelHabitClick(() => {
+      View.clearAddHabitForm();
+      View.hideAddHabitModal();
+    });
   },
-
-  handleUserToolButtons() {
-    const addHabitBtn = document.querySelector('.user-add-habit');
-    const filterBtn = document.querySelector('.user-filters');
-
-    const overlay = document.querySelector('.modal-overlay')
-    const modal = document.querySelector('.modal')
-
-    addHabitBtn.addEventListener('click', () => {
-      overlay.classList.toggle("hidden")
-      modal.classList.toggle("hidden")
-    })
-
-    filterBtn.addEventListener('click', () => {
-      
-    })
-  },
-
-  handleModalClose() {
-    const overlay = document.querySelector('.modal-overlay')
-    const modal = document.querySelector('.modal')
-
-    overlay.addEventListener('click', () => {
-      overlay.classList.toggle("hidden")
-      modal.classList.toggle("hidden")
-    })
-
-    modal.addEventListener('keypress', (event) => {
-      let key = event.key
-      console.log(key)
-    })
-  },
-}
+};
 
 export default Controller;
