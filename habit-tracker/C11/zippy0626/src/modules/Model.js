cb4e7f592cc8b7ef348e7fa2f1aad23aebc95583
zippy0626 <zippy0626@gmail.com>
2025-08-02 23:03:04 -0400

    added createHabit method, small changes

--- habit-tracker/C11/zippy0626/src/modules/Model.js
+++ habit-tracker/C11/zippy0626/src/modules/Model.js
@@ -1,33 +1,50 @@
-export default class Model {
-  constructor() {
-    this.initStorage();
-  }
+import { QuantitativeHabit, BooleanHabit } from "./habit.js";
 
+const Model = {
   initStorage() {
     if (!localStorage.getItem("all-habits")) {
       localStorage.setItem("all-habits", JSON.stringify([]));
     }
-  }
+  },
+
+  createHabit(habitData) {
+    // Quantitative Habit
+    // add quantity format to modal form !!! use logic in view
+    if (habitData["habitType"] === "number") {
+      const habit = new QuantitativeHabit(
+        habitData["habitName"],
+        habitData["habitDesc"],
+        habitData["habitQuantityFormat"],
+        0,
+        habitData["habitCategory"]
+      );
+      return habit;
+    } else {
+      // Completion Habit
+      const habit = new BooleanHabit(habitData["habitName"], habitData["habitDesc"], 0, habitData["habitCategory"]);
+      return habit;
+    }
+  },
 
   getAllHabits() {
     const data = localStorage.getItem("all-habits");
     return data ? JSON.parse(data) : undefined;
-  }
+  },
 
   setHabit(...habits) {
     // update
     const allHabits = this.getAllHabits();
     for (const habit of habits) {
-      let i = allHabits.findIndex((h) => h.name === habit.name)
+      let i = allHabits.findIndex((h) => h.name === habit.name);
       if (i !== -1) {
-        allHabits[i] = habit
+        allHabits[i] = habit;
       } else {
-        allHabits.push(habit)
+        allHabits.push(habit);
       }
     }
 
     localStorage.setItem("all-habits", JSON.stringify(allHabits));
-  }
+  },
 
   getHabit(name) {
     // read
@@ -46,17 +63,19 @@ export default class Model {
     }
 
     return undefined;
-  }
+  },
 
   deleteHabit(name) {
-    const allHabits = this.getAllHabits()
-    let i = allHabits.findIndex((habit) => habit.name === name)
+    const allHabits = this.getAllHabits();
+    let i = allHabits.findIndex((habit) => habit.name === name);
     if (i !== -1) {
-      allHabits.splice(i, 1)
+      allHabits.splice(i, 1);
     } else {
-      return undefined
+      return undefined;
     }
 
-    localStorage.setItem("all-habits", JSON.stringify(allHabits))
-  }
-}
+    localStorage.setItem("all-habits", JSON.stringify(allHabits));
+  },
+};
+
+export default Model;
