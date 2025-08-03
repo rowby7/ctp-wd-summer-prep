befeac6b697d0bdec342cf570adcc5eb8383d16a
zippy0626 <zippy0626@gmail.com>
2025-08-02 23:30:31 -0400

    refactor View to group methods within its own properties

--- habit-tracker/C11/zippy0626/src/modules/View.js
+++ habit-tracker/C11/zippy0626/src/modules/View.js
@@ -1,73 +1,58 @@
 const View = {
-  getSearchBar() {
-    return document.querySelector("#search-bar");
+  query(selector) {
+    return document.querySelector(selector);
   },
 
-  getAddHabitBtn() {
-    return document.querySelector(".user-add-habit");
-  },
-
-  getFiltersBtn() {
-    return document.querySelector(".user-filters");
-  },
-
-  getModalOverlay() {
-    return document.querySelector(".modal-overlay");
-  },
-
-  getCreateHabitBtn() {
-    return document.querySelector(".create-habit-btn");
-  },
-
-  getCancelHabitBtn() {
-    return document.querySelector(".cancel-habit-btn");
-  },
-
-  getAddHabitFormData() {
-    const d = new FormData(document.getElementById("add-habit-modal-form"))
-    const data = {}
-    for (const pair of d.entries()) {
-      let key = pair[0]
-      let val = pair[1]
-      data[key] = val
-    }
-    return data
-  },
-
-  showAddHabitModal() {
-    const addHabitModal = document.querySelector(".add-habit-modal");
-    const overlay = document.querySelector(".modal-overlay");
-    overlay.classList.remove("hidden");
-    addHabitModal.classList.remove("hidden");
-  },
-
-  hideAddHabitModal() {
-    const addHabitModal = document.querySelector(".add-habit-modal");
-    const overlay = document.querySelector(".modal-overlay");
-    overlay.classList.add("hidden");
-    addHabitModal.classList.add("hidden");
-  },
-
-  clearAddHabitForm() {
-    document.getElementById("add-habit-modal-form").reset();
-  },
-
-  // event listener callback fns for Controller
-  // handle add-habit modal related
   onAddHabitClick(callback) {
-    this.getAddHabitBtn().addEventListener("click", callback);
+    this.query(".user-add-habit").addEventListener("click", callback);
   },
 
   onModalOverlayClick(callback) {
-    this.getModalOverlay().addEventListener("click", callback);
+    this.query(".modal-overlay").addEventListener("click", callback);
   },
 
-  onCreateHabitClick(callback) {
-    this.getCreateHabitBtn().addEventListener("click", callback);
-  },
+  // group related methods in object properties
+  addHabitModal: {
+    query(selector) {
+      return document.querySelector(selector);
+    },
 
-  onCancelHabitClick(callback) {
-    this.getCancelHabitBtn().addEventListener("click", callback);
+    show() {
+      const addHabitModal = this.query(".add-habit-modal");
+      const overlay = this.query(".modal-overlay");
+      overlay.classList.remove("hidden");
+      addHabitModal.classList.remove("hidden");
+    },
+
+    hide() {
+      const addHabitModal = this.query(".add-habit-modal");
+      const overlay = this.query(".modal-overlay");
+      overlay.classList.add("hidden");
+      addHabitModal.classList.add("hidden");
+    },
+
+    clear() {
+      this.query("#add-habit-modal-form").reset();
+    },
+
+    getFormData() {
+      const d = new FormData(document.getElementById("add-habit-modal-form"));
+      const data = {};
+      for (const pair of d.entries()) {
+        let key = pair[0];
+        let val = pair[1];
+        data[key] = val;
+      }
+      return data;
+    },
+
+    onSubmitClick(callback) {
+      this.query(".create-habit-btn").addEventListener("submit", callback);
+    },
+
+    onCancelClick(callback) {
+      this.query(".cancel-habit-btn").addEventListener("click", callback);
+    },
   },
 };
 
