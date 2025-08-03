85daf948659c4ec22775a0fb8b881f0660b3958b
zippy0626 <zippy0626@gmail.com>
2025-08-03 15:27:10 -0400

    added population of completion history

--- habit-tracker/C11/zippy0626/src/modules/habit.js
+++ habit-tracker/C11/zippy0626/src/modules/habit.js
@@ -8,7 +8,18 @@ class QuantitativeHabit {
     this.quantity = quantity;
     this.category = category;
     this.createdDate = format(new Date(), "yyyy-MM-dd");
-    this.completionHistory = {}
+    this.completionHistory = {};
+
+    // Populate completion history
+    let year = new Date().getFullYear();
+    let date = new Date(year, 0, 1);
+    let end = new Date(year, 11, 31);
+
+    while (date <= end) {
+      const dateStr = format(date, "yyyy-MM-dd");
+      this.completionHistory[dateStr] = { completed: false, quantity: 0 };
+      date.setDate(date.getDate() + 1);
+    }
   }
 
   changeQuantityFormat(newFormat) {
@@ -31,7 +42,18 @@ class BooleanHabit {
     this.timesCompleted = timesCompleted;
     this.category = category;
     this.createdDate = format(new Date(), "yyyy-MM-dd");
-    this.completionHistory = {}
+    this.completionHistory = {};
+
+    // Populate completion history
+    let year = new Date().getFullYear();
+    let date = new Date(year, 0, 1);
+    let end = new Date(year, 11, 31);
+
+    while (date <= end) {
+      const dateStr = format(date, "yyyy-MM-dd");
+      this.completionHistory[dateStr] = { completed: false };
+      date.setDate(date.getDate() + 1);
+    }
   }
 
   increaseTimesCompleted(amount) {
