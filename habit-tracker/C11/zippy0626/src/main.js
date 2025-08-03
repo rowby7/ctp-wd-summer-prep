3b8597c1aef51a23dd05ffb2222cc82b951e616a
zippy0626 <zippy0626@gmail.com>
2025-08-03 15:28:13 -0400

    cleaned up stuff

--- habit-tracker/C11/zippy0626/src/main.js
+++ habit-tracker/C11/zippy0626/src/main.js
@@ -1,2 +1,7 @@
-import '/src/assets/style.css';
-import '/src/assets/media-queries.css';
+import "/src/assets/style.css";
+import "/src/assets/media-queries.css";
+import Controller from "./modules/Controller.js";
+import Model from "./modules/Model.js";
+
+Controller.init();
+
