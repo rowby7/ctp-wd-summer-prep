20c436f9da51fcebc52935ecb39d40e4fc0db4d2
zippy0626 <zippy0626@gmail.com>
2025-08-06 00:31:13 -0400

    minor changes

--- habit-tracker/C11/zippy0626/src/main.js
+++ habit-tracker/C11/zippy0626/src/main.js
@@ -1,7 +1,5 @@
 import "/src/assets/style.css";
 import "/src/assets/media-queries.css";
 import Controller from "./modules/Controller.js";
-import Model from "./modules/Model.js";
 
 Controller.init();
-
